40a10b5b85928522730f22ba9353a89f46f18401
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-21 16:40:28 +0300

    update navigate

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/tweets" element={<TweetsPage />} />
-        <Route path="*" element={<Navigate to="/tweets" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
